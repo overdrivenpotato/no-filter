1026de4f379485446cb4dbbbbd02fa57667d0f36
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2017-02-13 15:08:08 -0500

    Initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,65 @@
+import webpack from 'webpack'
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const htmlMinifyOptions = {
+    collapseWhitespace: true,
+    removeComments: true,
+}
+
+const commonPlugins = [
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    new HtmlWebpackPlugin({
+        template: 'index.html',
+        minify: htmlMinifyOptions,
+    }),
+]
+
+const productionPlugins = [
+    new webpack.NoEmitOnErrorsPlugin(),
+]
+
+const plugins = process.env.NODE_ENV === 'production'
+    ? commonPlugins.concat(productionPlugins)
+    : commonPlugins
+
+const config = {
+    bail: true,
+    context: path.join(__dirname, 'src'),
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.join(__dirname, 'build'),
+    },
+    entry: [
+        'react-hot-loader/patch',
+        './main.jsx',
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                enforce: 'pre',
+                use: [ 'eslint-loader' ],
+            },
+            {
+                test: /\.jsx?$/,
+                loaders: ['react-hot-loader/webpack', 'babel-loader'],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: plugins,
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    stats: {
+        // Suppress html-webpack-plugin logspam
+        children: false,
+    },
+}
+
+export default config
