81a01fefdad295e65c2ce8c9770d221a130d36ed
azemcetiner <azem_cetiner@hotmail.com>
2017-03-14 17:08:59 -0400

    test

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import { render } from 'react-dom'
 import App from './components/app'
 
 render(
-  <App text='Hello World!' />,
+  <App text='Marko is a cool cat!' />,
   document.getElementById('root')
 )
