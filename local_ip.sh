a4728869fa11432039c3ce07590e2f56ff9f685b
Marko Mijalkovic <marko.mijalkovic97@gmail.com>
2017-04-10 11:39:15 -0400

    Grab last ip in local_ip.sh

--- local_ip.sh
+++ local_ip.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Get the local IP so we can pass it into react native for the server location.
-ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
+ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | tail -1
