4775f150c1259892ca3109ef99cf20e895040d50
Jack Zavarella <jackzavarella@Jacks-MacBook-Air.local>
2017-04-10 18:41:32 -0400

    Fixed linting errors

--- src/app/components/register.js
+++ src/app/components/register.js
@@ -11,21 +11,21 @@ import LoadingSpinner from './LoadingSpinner'
 
 const styles = {
   component: {
-    flex: 1
+    flex: 1,
   },
   base: {
     title: {
       elevation: 2,
       backgroundColor: commonColors.DARK_BG,
-      marginBottom: 30
-    }
+      marginBottom: 30,
+    },
 
   },
   text: {
     fontSize: 25,
     alignSelf: 'center',
     color: commonColors.RED,
-    padding: 30
+    padding: 30,
   },
   input: {
     marginLeft: 40,
@@ -34,8 +34,8 @@ const styles = {
   },
   errorText: {
     color: commonColors.RED,
-    alignSelf: 'center'
-  }
+    alignSelf: 'center',
+  },
 }
 
 class LoginForm extends Component {
@@ -45,83 +45,107 @@ class LoginForm extends Component {
     password: '',
     cnfmpassword: '',
     error: '',
-    loading: false
+    loading: false,
   }
 
-  sendRegister() {
+  sendRegister = () => {
     const { email, password, cnfmpassword } = this.state
 
     Keyboard.dismiss()
 
     this.setState({ error: '', loading: true })
 
-    if(password != cnfmpassword) {
+    if (password !== cnfmpassword) {
       this.setState({ error: 'Passwords do not match', loading: false })
     } else {
       firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(this.onRegisterSuccess.bind(this))
-        .catch(this.displayError.bind(this))
+        .then(this.onRegisterSuccess)
+        .catch(this.displayError)
     }
-
   }
 
-  redirectLogin() {
+  redirectLogin = () => {
     /*
       TODO some logic to redirect to the login screen
     */
   }
 
-  onRegisterSuccess() {
+  onRegisterSuccess = () => {
     this.setState({ error: 'Registered!', loading: false })
     /*
       TODO logic to execute when the user registers
     */
   }
 
-  displayError() {
+  displayError = () => {
     this.setState({ error: 'There was an error registering', loading: false })
   }
 
-  renderRegisterButton() {
-    if(this.state.loading) {
+  renderRegisterButton () {
+    if (this.state.loading) {
       return (
         <LoadingSpinner size={'small'} />
       )
     }
     return (
-      <Button onPress={this.sendRegister.bind(this)} children='register'>
+      <Button onPress={this.sendRegister} children='register'>
         Register
       </Button>
     )
   }
 
-  renderLoginButton() {
-    if(!this.state.loading) {
+  renderLoginButton () {
+    if (!this.state.loading) {
       return (
-        <Button onPress={this.redirectLogin.bind(this)} children='have an account'>
+        <Button onPress={this.redirectLogin} children='have an account'>
           Have an account?
         </Button>
       )
     }
-
   }
 
-  render() {
+  render () {
     return (
-      <View style={ styles.component }>
-        <View style={ styles.base.title }>
-          <Text style={ styles.text }>
+      <View style={styles.component}>
+        <View style={styles.base.title}>
+          <Text style={styles.text}>
             Register An Account
           </Text>
 
         </View>
-        <Text style={ styles.errorText }>
+        <Text style={styles.errorText}>
           {this.state.error}
         </Text>
-        <TextInput value={this.state.name} onChangeText={name => this.setState({ name })} style={ styles.input } placeholder='full name' autoCorrect={false} />
-        <TextInput value={this.state.email} onChangeText={email => this.setState({ email })} style={ styles.input } placeholder='email' autoCorrect={false} />
-        <TextInput value={this.state.password} onChangeText={password => this.setState({ password })} style={ styles.input } placeholder='password' autoCorrect={false} secureTextEntry={true}/>
-        <TextInput value={this.state.cnfmpassword} onChangeText={cnfmpassword => this.setState({ cnfmpassword })} style={ styles.input } placeholder='confirm password' autoCorrect={false} secureTextEntry={true}/>
+        <TextInput
+          value={this.state.name}
+          onChangeText={name => this.setState({name})}
+          style={styles.input}
+          placeholder='full name'
+          autoCorrect={false}
+        />
+        <TextInput
+          value={this.state.email}
+          onChangeText={email => this.setState({email})}
+          style={styles.input}
+          placeholder='email'
+          autoCorrect={false}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={password => this.setState({password})}
+          style={styles.input}
+          placeholder='password'
+          autoCorrect={false}
+          secureTextEntry
+        />
+        <TextInput
+          value={this.state.cnfmpassword}
+          onChangeText={cnfmpassword => this.setState({cnfmpassword})}
+          style={styles.input}
+          placeholder='confirm password'
+          autoCorrect={false}
+          secureTextEntry
+        />
         {this.renderRegisterButton()}
         {this.renderLoginButton()}
       </View>
